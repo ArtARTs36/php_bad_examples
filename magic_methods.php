56047d9c834df6848ea551986af3025f6efe170e
aukrainskiy <aukrainskiy@phoenixit.ru>
2021-04-23 12:44:46 +0300

    php magic methods

---
+++ magic_methods.php
@@ -0,0 +1,17 @@
+<?php
+
+class User
+{
+    public $name = 'Test';
+
+    private $email = 'test@mail.ru';
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}
+
+$user = new User();
+
+var_dump($user->email);
